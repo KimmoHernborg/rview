4974b4e50afc3c7ac0b6b264acdcef69975b32d6
Kimmo Hernborg <x-kihe@oresundsbron.com>
2025-08-07 11:44:54 +0200

    Change React Query stale/gc timeouts and don't refetch on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import "./index.css";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      gcTime: 10 * 60 * 1000, // 10 minutes
+      staleTime: 60 * 60 * 1000, // 1 hour
+      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+      refetchOnWindowFocus: false,
     },
   },
 });
